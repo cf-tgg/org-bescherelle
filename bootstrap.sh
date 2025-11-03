c530c4bd049296337c108f3117736498e6e5bbfe
cf <cf.gg.tty@hotmail.com>
2025-11-03 14:37:52 -0500

    fix: installation initiale
    ajout option `-I` pour INSTALL_ONLY

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -xe
+
+conjuge=$(find . -type f -name "conjuge" -exec realpath {} \;)
+[ -x "$conjuge" ] || { printf '[ERROR] CMD not executable: %s\n' "$conjuge" >&2 ; exit 1 ; }
+verbes=$(find . -type f -name "verbes" -exec realpath {} \;)
+[ -f "$verbes" ] || { printf '[ERROR] CMD no such file: %s\n' "$verbes" >&2 ; exit 1 ; }
+BESCHERELLE_DIR=${BESCHERELLE_DIR:-"$HOME/Documents/Bescherelle"}
+( [ ! -d "$BESCHERELLE_DIR" ] && mkdir -p "$BESCHERELLE_DIR") || { printf '[ERROR] CMD could not create directory: %s\n' "$BESCHERELLE_DIR" >&2 ; exit 1 ; }
+[ -x "$(command -v parallel)" ] && parallel=$(which parallel) || { printf '[ERROR] CMD: could not find parallel executable on PATH\n' >&2 ; exit 1 ; }
+
+hul=$(ulimit -Hn) ul=$((hul/2))
+ulimit -n ${ul}
+
+$parallel --progress --verbose -j0 --joblog "$BESCHERELLE_DIR/bescherelle-bootstrap.log" $conjuge -vI {} :::: "$verbes"
